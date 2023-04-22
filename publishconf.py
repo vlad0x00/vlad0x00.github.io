dc9a7763a3c95977373b6d70d117b59ee994e7c6
vlad0x00 <vladlich@protonmail.com>
2023-04-21 22:44:37 -0700

    Update

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://vlad0x00.github.io'
+SITEURL = 'vlich.net'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
